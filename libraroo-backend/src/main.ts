ca24a20340ffe297b411998ea5ea8dbaa6d51faf
Ido Rosenberger <ido.ross98@gmail.com>
2024-12-03 20:38:24 +0200

    Add swagger

--- libraroo-backend/src/main.ts
+++ libraroo-backend/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Libraroo API ')
+    .setDescription('donfil')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
