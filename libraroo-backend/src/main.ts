a47cb8f3a92e06ac7e18aea38e5d60d2072fe74d
Ziv Siag <ziv.siag@gmail.com>
2024-12-04 17:24:44 +0200

    format

--- libraroo-backend/src/main.ts
+++ libraroo-backend/src/main.ts
@@ -4,7 +4,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
   const config = new DocumentBuilder()
     .setTitle('Libraroo API ')
     .setDescription('this is api')
