c09b58d8bf682fc947bde6b36f4936e27637cd77
Nadavrav <nadavrav@post.bgu.ac.il>
2024-12-12 16:40:14 +0200

    fixed ido's lies

--- libraroo-web/src/App.tsx
+++ libraroo-web/src/App.tsx
@@ -1,5 +1,11 @@
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter,
+  Navigate,
+  Outlet,
+  Route,
+  Routes,
+} from "react-router-dom";
 import { Register } from "./users/components/Register";
 import Login from "./users/components/Login";
 import { CreateBookCatalog } from "./bookCatalog/components/CreateBookCatalog";
@@ -10,11 +16,27 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<ProtectedRoute><TextField/></ProtectedRoute>} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <TextField />
+            </ProtectedRoute>
+          }
+        />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/book-catalog/add-book" element={<CreateBookCatalog />} />
-
+        <Route
+          path="book-catalog/*"
+          element={
+            <ProtectedRoute>
+              <Outlet />
+            </ProtectedRoute>
+          }
+        >
+          <Route path="add-book" element={<CreateBookCatalog />} />
+          <Route index element={<Navigate to="list" />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
