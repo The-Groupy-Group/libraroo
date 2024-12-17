e4c2160925bd85b66f56e34eb5b8a19ab4c5e676
Ido Rosenberger <ido.ross98@gmail.com>
2024-12-17 18:12:09 +0200

    lint

--- libraroo-web/src/bookCatalog/components/CreateBookCatalog.tsx
+++ libraroo-web/src/bookCatalog/components/CreateBookCatalog.tsx
@@ -27,7 +27,7 @@ export const CreateBookCatalog: React.FC = () => {
 
       navigate("/book-catalog/list");
     } catch (err) {
-      let error = await Utils.getErrorMessage(err as Error);
+      const error = await Utils.getErrorMessage(err as Error);
       setError(error);
     } finally {
       setLoading(false);
